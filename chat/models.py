3357f8c7aca4bd381cac6998d76d9fe01fcb8224
Abdualwasa12 <62475391+Abdualwasa12@users.noreply.github.com>
2023-10-22 09:17:49 +0300

    first commit

---
+++ chat/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+from account.models import User
+
+class Message(models.Model):
+    body =models.TextField()
+    sent_by = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    
+    class Meta:
+        ordering = ['created_at' ]
+        
+    def __str__(self):
+        return f'{self.sent_by}' 
+    
+    
+class Room(models.Model):
+    WITTING = 'waiting'
+    ACTIVE = 'active'
+    CLOSED = 'closed'
+    
+    CHOICES_STATUS = {
+    (WITTING , 'Waiting'),
+    (ACTIVE ,'Active'),
+    (CLOSED ,'Closed'),
+        
+    }
+    
+    uuid = models.CharField(max_length=100)
+    client = models.CharField(max_length=200)
+    agent = models.ForeignKey(User, related_name='rooms', blank=True,null=True, on_delete=models.SET_NULL)
+    messages = models.ManyToManyField(Message, blank=True)
+    url = models.CharField(max_length=200,blank=True,null=True)
+    status = models.CharField(max_length=20, choices=CHOICES_STATUS, default=WITTING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at' ]
+        
+    def __str__(self):
+        return f'{self.client} - {self.uuid}' 
+    
+    
+    
+    
+    
+    
+    
